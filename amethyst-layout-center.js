6bfb7cf4e253921b83fd1dbf8aaae010bab5747d
Tom M G <devbytom@gmail.com>
2024-07-26 13:16:47 +0200

    Improve Amethyst center layout

--- amethyst-layout-center.js
+++ amethyst-layout-center.js
@@ -2,11 +2,11 @@ function layout() {
     return {
         name: "Center",
         getFrameAssignments: (windows, screenFrame) => {
-            const widthMuliplier = 0.6;
-            const heightMultiplier = 0.96;
+            const widthMuliplier = 0.76;
+            const heightMultiplier = 0.98;
 
             frame = {
-                x: screenFrame.x + screenFrame.width * (1 - widthMuliplier) / 2,
+                x: screenFrame.x + screenFrame.width * (1 - widthMuliplier) / 1.9,
                 y: screenFrame.y + screenFrame.height * (1 - heightMultiplier) / 2,
                 width: screenFrame.width * widthMuliplier,
                 height: screenFrame.height * heightMultiplier,
