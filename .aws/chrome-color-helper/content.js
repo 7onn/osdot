e886543d8809bd48f8d5c7064dff273a1e4f8710
Tom M G <devbytom@gmail.com>
2024-11-15 15:58:21 +0100

    fix aws color plugin

--- .aws/chrome-color-helper/content.js
+++ .aws/chrome-color-helper/content.js
@@ -1,11 +1,15 @@
-const accountId = document
-  .querySelector('button[data-testid="awsc-copy-accountid"]')
-  .previousElementSibling.innerText.replace(/\-/g, "");
+const metaTag = document.querySelector('meta[name="awsc-session-data"]');
+const content = metaTag.getAttribute('content');
+const sessionData = JSON.parse(content);
+const accountId = sessionData.accountId;
 
 color = accountColors[accountId];
-
-if (color !== undefined) {
-  document.querySelector("#awsc-nav-header>nav").style.backgroundColor = color;
-} else {
+if (!color) {
   console.log("Account color not found");
+} else {
+  setTimeout(() => {
+    document.querySelector("#awsc-top-level-nav").style.backgroundColor = color;
+  }, 1000 * 5)
 }
+
+
